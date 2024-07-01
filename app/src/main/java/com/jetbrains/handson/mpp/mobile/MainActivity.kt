452c01c1bfe748f66494001eb2cdbb973d500fe6
Luke Edwards <cm22683@bristol.ac.uk>
2024-07-01 17:21:10 +0100

    feat!: implement inner class for spinner

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -2,6 +2,10 @@ package com.jetbrains.handson.mpp.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
@@ -12,9 +16,39 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+
+        val spinner: Spinner = findViewById(R.id.departure_spinner) // Create an ArrayAdapter using the string array and a default spinner layout.
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var v = parent?.getItemAtPosition(position).toString()
+            }
+        }
+
+        ArrayAdapter.createFromResource(this, R.array.stations, android.R.layout.simple_spinner_item
+        ).also { adapter -> // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
+
+
+//        val arrival_spinner: Spinner = findViewById(R.id.arrival_spinner) // Create an ArrayAdapter using the string array and a default spinner layout.
+//        ArrayAdapter.createFromResource(this, R.array.stations, android.R.layout.simple_spinner_item
+//        ).also { adapter -> // Specify the layout to use when the list of choices appears.
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // Apply the adapter to the spinner.
+//            arrival_spinner.adapter = adapter
+//        }
+
+
+
     }
 
     override fun setLabel(text: String) {
         findViewById<TextView>(R.id.main_text).text = text
     }
+
+
+
 }

cc53123ad4516d34eee77549a811b6c8af0dc180
Luke Edwards <cm22683@bristol.ac.uk>
2024-07-02 09:16:23 +0100

    feat: LiveTrainTimeFetcher setter methods

--- SharedCode/src/commonMain/kotlin/LiveTrainTimeFetcher.kt
+++ SharedCode/src/commonMain/kotlin/LiveTrainTimeFetcher.kt
@@ -1,4 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 
 class LiveTrainTimeFetcher {
+    private lateinit var departureStation : Station
+    private lateinit var arrivalStation : Station
+
+    fun setDepartureStation(station : Station) {
+        this.departureStation = station
+    }
+
+    fun setArrivalStation(station : Station) {
+        this.arrivalStation = station
+    }
 }

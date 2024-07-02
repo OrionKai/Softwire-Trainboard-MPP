663550134ced0ba66c4164f58e824b7ad8b90736
Luke Edwards <cm22683@bristol.ac.uk>
2024-07-02 08:34:37 +0100

    feat: created list of Stations and parsed into the adapter

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -25,19 +25,18 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val spinner: Spinner = findViewById(R.id.departure_spinner) // Create an ArrayAdapter using the string array and a default spinner layout.
 
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOfStations.map { it.getName() })
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {}
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                var v = parent?.getItemAtPosition(position).toString()
+                val selectedStationName = parent?.getItemAtPosition(position).toString()
             }
-        }
-
-        //ArrayAdapter<Station> adapter
-
-        ArrayAdapter.createFromResource(this, R.array.stations, android.R.layout.simple_spinner_item
-        ).also { adapter -> // Specify the layout to use when the list of choices appears.
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // Apply the adapter to the spinner.
-            spinner.adapter = adapter
         }
 
 
